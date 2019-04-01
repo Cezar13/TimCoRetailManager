851fb97380b8b2f3b13100d3e9fd65e065571a33
Cezar <filache.cezar@gmail.com>
2019-04-01 15:10:54 +0100

    getting user data partial

---
+++ TRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDataManager.Library.DataAccess
+{
+   public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+
+        }
+    }
+}
